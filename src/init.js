a6afacfb242d4dd94f8d256d0ba0be7250ad7efb
namrata <rathiNamrata@github.com>
2024-03-29 12:22:11 +0100

    hex-color

--- src/init.js
+++ src/init.js
@@ -1,1 +1,26 @@
 alert('init.js loaded!');
+
+const data = {
+    character: [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+}
+
+const dom = {
+    body : document.querySelector('body'),
+    display : document.querySelector('.display'),
+    btnColor : document.getElementById('color-change-btn')
+}
+
+function generateRandomcolor() {
+    let color = '#' ;
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * data.character.length);
+        color += data.character[randomIndex];
+    }
+    return color;
+}
+
+dom.btnColor.addEventListener('click', function() {
+    const randomColor = generateRandomcolor();
+    dom.body.style.backgroundColor = randomColor;
+    dom.display.innerText = ` Hex code :${randomColor}` ;
+})
